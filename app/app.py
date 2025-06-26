e1af7861f70c57a3423cf6665aa12e31332ca8db
vishaldeshmukh2k6 <vishaldeshmuk143@gmail.com>
2025-06-26 11:18:02 +0530

    Updated files and added new templates

--- app/app.py
+++ app/app.py
@@ -1,8 +1,79 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
+from models import db, User, Note
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
-def home():
-    return render_template('base.html')
+@login_required
+def index():
+    notes = Note.query.filter_by(user_id=current_user.id).all()
+    return render_template('index.html', notes=notes)
 
+@app.route('/add', methods=['POST'])
+@login_required
+def add_note():
+    content = request.form['content']
+    new_note = Note(content=content, user_id=current_user.id)
+    db.session.add(new_note)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+@login_required
+def delete_note(id):
+    note = Note.query.get_or_404(id)
+    if note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+    return redirect('/')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful. Please login.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and bcrypt.check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
